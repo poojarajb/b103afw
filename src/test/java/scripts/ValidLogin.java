8486f9b8a86528aecd92bbdfbf5029c98cc724b9
poojarajb <poojarajb@gmail.com>
2024-07-09 21:25:23 -0500

    updated login page code

--- src/test/java/scripts/ValidLogin.java
+++ src/test/java/scripts/ValidLogin.java
@@ -4,19 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
-import Page.HomePage;
-import Page.LoginPage;
+import generic.Utility;
+import page.HomePage;
+import page.LoginPage;
 
 public class ValidLogin extends BaseTest
 {
 	@Test(priority = 1)
 	public void testValidLogin()
 	{
+		String un = Utility.getExcelData(EXCEL_PATH, "ValidLogin",1, 0);
+		String pw = Utility.getExcelData(EXCEL_PATH, "ValidLogin",1, 1);
+		
 //		1. enter valid user name
 		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName("admin");
+		loginPage.setUserName(un);
 //		2. enter valid password
-		loginPage.setPassword("pointofsale2");
+		loginPage.setPassword(pw);
 //		3. click on go button
 		loginPage.clickGoButton();
 //		4. verify home page is displayed
